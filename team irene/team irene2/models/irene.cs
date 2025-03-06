f4f593770e9586c736a0cfdec7da7d03d7b1753c
school-jess <jessmathew.evangelista@dbtc-cebu.edu.ph>
2025-03-06 13:58:13 +0800

    added model

--- team irene/team irene2/models/irene.cs
+++ team irene/team irene2/models/irene.cs
@@ -1,12 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace team_irene2.models
+﻿namespace team_irene2.models
 {
     public class irene
     {
+        public string fullName {get {
+            return $"{firstName} {middleInitial} {lastName}";
+        }}
+
+        public int age {get {
+            return 
+        }}
+        public string address {get {
+            return $"{houseNumber} {street}, {barangay}, {city}, {province}, {country}";
+        }}
+        
+        public string firstName {get; set;}
+        public string middleInitial {get; set;}
+        public string lastName {get; set;}
+        public string suffix {get; set;}
+
+        public string birthDay {get; set;}
+
+        public string houseNumber {get; set;}
+        public string street {get; set;}
+        public string barangay {get; set;}
+        public string province {get; set;}
+        public string city {get; set;}
+        public string country {get; set;}
     }
 }
