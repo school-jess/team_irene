3004092e81a5a10eea130b1bbdf8351ab6783691
school-jess <jessmathew.evangelista@dbtc-cebu.edu.ph>
2025-03-20 14:56:40 +0800

    starting to create db context

--- basic_information_library/database.cs
+++ basic_information_library/database.cs
@@ -1,93 +1,21 @@
-﻿using MySql.Data.MySqlClient;
-using basic_information_library.models;
+﻿using basic_information_library.models;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 namespace basic_information_library;
 
-public class Database
+public class DatabaseCtx : DbContext
 {
-    MySqlConnection conn { get; set; }
-    IConfigurationRoot config;
+    private IConfigurationRoot config;
 
-    public Database()
+    public DatabaseCtx()
     {
-        config = new ConfigurationBuilder().AddUserSecrets<Database>().Build();
+        config = new ConfigurationBuilder().AddUserSecrets<DatabaseCtx>().Build();
     }
+    public DbSet<BasicInformation> personalDetails { get; set; }
 
-    public void Connect(string user)
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
-        string connStr = $"Server=localhost; database=first_database; user={user}; password={config["MYSQL_ROOT_PASSWORD"]};";
-        conn = new MySqlConnection(connStr);
-        conn.Open();
-    }
-
-    public void Insert(BasicInformation model)
-    {
-        MySqlCommand cmd = new MySqlCommand("INSERT INTO first_table (first_name, middle_initial, last_name, suffix, full_name, birthday, age, house_number, street_name, barangay, city, country, full_address, province) value (@firstName, @middleInitial, @lastName, @suffix, @fullName, @birthday, @age, @houseNumber, @street, @barangay, @city, @country, @address, @province)", conn);
-        cmd.Parameters.AddWithValue("@firstName", model.firstName);
-        string? middleInitial = null;
-        if (model.middleInitial != "")
-        {
-            middleInitial = model.middleInitial;
-        }
-        cmd.Parameters.AddWithValue("@middleInitial", middleInitial);
-        cmd.Parameters.AddWithValue("@lastName", model.lastName);
-        string? suffix = null;
-        if (model.suffix != "")
-        {
-            suffix = model.suffix;
-        }
-        cmd.Parameters.AddWithValue("@suffix", suffix);
-        cmd.Parameters.AddWithValue("@fullName", model.fullName);
-        cmd.Parameters.AddWithValue("@birthday", model.birthday);
-        cmd.Parameters.AddWithValue("@age", model.age);
-        cmd.Parameters.AddWithValue("@houseNumber", model.houseNumber);
-        cmd.Parameters.AddWithValue("@street", model.street);
-        cmd.Parameters.AddWithValue("@barangay", model.barangay);
-        cmd.Parameters.AddWithValue("@city", model.city);
-        cmd.Parameters.AddWithValue("@country", model.country);
-        cmd.Parameters.AddWithValue("@address", model.address);
-        cmd.Parameters.AddWithValue("@province", model.province);
-        int rowsAffected = cmd.ExecuteNonQuery();
-        Console.WriteLine("Saved to database");
-        Console.WriteLine($"{rowsAffected} row(s) inserted");
-    }
-
-    public MySqlDataReader Select(string? fullName)
-    {
-        string sqlStr = "SELECT * FROM first_table";
-        if (fullName != null)
-        {
-            sqlStr += $" WHERE full_name=@fullName";
-        }
-        MySqlCommand cmd = new MySqlCommand(sqlStr, conn);
-        if (fullName != null)
-        {
-            cmd.Parameters.AddWithValue("@fullName", fullName);
-        }
-        MySqlDataReader reader = cmd.ExecuteReader();
-        return reader;
-    }
-
-    public void Remove(string fullName)
-    {
-        if (fullName == "")
-        {
-            throw new Exception("Must full first name");
-        }
-        string sqlStr = $"DELETE FROM first_table WHERE full_name=@fullName";
-        MySqlCommand cmd = new MySqlCommand(sqlStr, conn);
-        cmd.Parameters.AddWithValue("@fullName", fullName);
-        cmd.ExecuteNonQuery();
-    }
-
-    public void CloseReader(MySqlDataReader reader)
-    {
-        reader.Close();
-    }
-
-    ~Database()
-    {
-        conn.Close();
+        optionsBuilder.UseMySQL($"Server=localhost; Database=first_database; User=root; Password={config["MYSQL_ROOT_PASSWORD"]}");
     }
 }
--- basic_information_library/models/basic_information_model.cs
+++ basic_information_library/models/basic_information_model.cs
@@ -1,291 +1,288 @@
-namespace basic_information_library.models
+namespace basic_information_library.models;
+
+public class BasicInformation
 {
-    public class BasicInformation
+    public string fullName
     {
-        public string fullName
+        get
         {
-            get
+            string fullNameToRet = "";
+            fullNameToRet += $"{_firstName} ";
+            if (_middleInitial != "")
             {
-                string fullNameToRet = "";
-                fullNameToRet += $"{_firstName} ";
-                if (_middleInitial != "")
-                {
-                    fullNameToRet += $"{_middleInitial.ToUpper()}. ";
-                }
-                if (_suffix != "")
-                {
-                    fullNameToRet += $"{_lastName} ";
-                    fullNameToRet += _suffix;
-                }
-                else
-                {
-                    fullNameToRet += _lastName;
-                }
-                return fullNameToRet;
+                fullNameToRet += $"{_middleInitial.ToUpper()}. ";
             }
-        }
-        public int age
-        {
-            get
+            if (_suffix != "")
             {
-                int age_shadow = DateTime.Now.Year - _birthDay.Year;
-                if (DateTime.Now < _birthDay.AddYears(age_shadow))
-                {
-                    age_shadow--; // Adjust if birthday hasn't occurred this year yet
-                }
-                return age_shadow;
+                fullNameToRet += $"{_lastName} ";
+                fullNameToRet += _suffix;
             }
-        }
-        public string address
-        {
-            get
+            else
             {
-                return $"{_houseNumber} {_street}, {_barangay}, {_city}, {_province}, {_country}";
+                fullNameToRet += _lastName;
             }
-        }
-
-        public string firstName
-        {
-            get => _firstName; set
-            {
-                if (value == "")
-                {
-                    throw new Exception("Must provide first name input");
-                }
-                if (value.Any(char.IsDigit))
-                {
-                    throw new Exception("Number inside of first name");
-                }
-                if (value.Length > 20)
-                {
-                    throw new Exception("first name too long. Must be less than or equal to 20");
-                }
-                _firstName = Capitalize(value);
-            }
-        }
-        private string _firstName;
-
-        public string middleInitial
-        {
-            get => _middleInitial; set
-            {
-                if (value.Any(char.IsDigit))
-                {
-                    throw new Exception("Number inside of middle initial");
-                }
-                if (value.Length > 10)
-                {
-                    throw new Exception("middle initial too long. Must be less than or equal to 10");
-                }
-                _middleInitial = Capitalize(value);
-            }
-        }
-
-        private string _middleInitial;
-
-        public string lastName
-        {
-            get => _lastName; set
-            {
-                if (value == "")
-                {
-                    throw new Exception("Must provide last name input");
-                }
-                if (value.Any(char.IsDigit))
-                {
-                    throw new Exception("Number inside of last name");
-                }
-                if (value.Length > 20)
-                {
-                    throw new Exception("last name too long. Must be less than or equal to 20");
-                }
-                _lastName = Capitalize(value);
-            }
-        }
-        public string _lastName;
-
-        public string suffix
-        {
-            get => _suffix; set
-            {
-                if (value.Any(char.IsDigit))
-                {
-                    throw new Exception("Number inside of suffix");
-                }
-                if (value.Length > 10)
-                {
-                    throw new Exception("suffix too long. Must be less than or equal to 10");
-                }
-                _suffix = value;
-            }
-        }
-
-        private string _suffix;
-
-        public string birthday
-        {
-            get => _birthDay.ToString("yyyy-MM-dd"); set
-            {
-                if (value == "")
-                {
-                    throw new Exception("Must provide birthday input");
-                }
-                string format = "yyyy-MM-dd";
-                _birthDay = DateTime.ParseExact(value, format, System.Globalization.CultureInfo.InvariantCulture);
-            }
-        }
-        public DateTime _birthDay;
-
-        public string houseNumber
-        {
-            get => _houseNumber; set
-            {
-                if (value == "")
-                {
-                    throw new Exception("Must provide house number input");
-                }
-                if (value.Length > 10)
-                {
-                    throw new Exception("suffix too long. Must be less than or equal to 10");
-                }
-                _houseNumber = value;
-            }
-        }
-        private string _houseNumber;
-
-        public string street
-        {
-            get => _street; set
-            {
-                if (value == "")
-                {
-                    throw new Exception("Must provide street input");
-                }
-                if (value.Any(char.IsDigit))
-                {
-                    throw new Exception("Number inside of street");
-                }
-                if (value.Length > 20)
-                {
-                    throw new Exception("suffix too long. Must be less than or equal to 20");
-                }
-                _street = Capitalize(value);
-            }
-        }
-        private string _street;
-
-        public string barangay
-        {
-            get => _barangay;
-            set
-            {
-                if (value == "")
-                {
-                    throw new Exception("Must provide barangay input");
-                }
-                if (value.Any(char.IsDigit))
-                {
-                    throw new Exception("Number inside of barangay");
-                }
-                if (value.Length > 20)
-                {
-                    throw new Exception("suffix too long. Must be less than or equal to 20");
-                }
-                _barangay = Capitalize(value);
-            }
-        }
-        private string _barangay;
-
-        public string city
-        {
-            get => _city;
-            set
-            {
-                if (value == "")
-                {
-                    throw new Exception("Must provide barangay input");
-                }
-                if (value.Any(char.IsDigit))
-                {
-                    throw new Exception("Number inside of city");
-                }
-                if (value.Length > 20)
-                {
-                    throw new Exception("suffix too long. Must be less than or equal to 20");
-                }
-                _city = Capitalize(value);
-            }
-        }
-        private string _city;
-
-        public string province
-        {
-            get => _province;
-
-            set
-            {
-                if (value == "")
-                {
-                    throw new Exception("Must provide province input");
-                }
-                if (value.Any(char.IsDigit))
-                {
-                    throw new Exception("Number inside of province");
-                }
-                if (value.Length > 20)
-                {
-                    throw new Exception("suffix too long. Must be less than or equal to 20");
-                }
-                _province = Capitalize(value);
-            }
-        }
-        private string _province;
-        public string country
-        {
-            get => _country; set
-            {
-                if (value == "")
-                {
-                    throw new Exception("Must provide country input");
-                }
-                if (value.Any(char.IsDigit))
-                {
-                    throw new Exception("Number inside of country");
-                }
-                if (value.Length > 20)
-                {
-                    throw new Exception("suffix too long. Must be less than or equal to 20");
-                }
-                _country = Capitalize(value);
-            }
-        }
-        private string _country;
-
-        private static string Capitalize(string origStr)
-        {
-            string strToRet = "";
-            bool isFirstChar = true;
-            for (int j = 0; j < origStr.Length; j++)
-            {
-                if (origStr[j] == ' ')
-                {
-                    isFirstChar = true;
-                    strToRet += " ";
-                    continue;
-                }
-                if (isFirstChar)
-                {
-                    strToRet += origStr[j].ToString().ToUpper();
-                    isFirstChar = false;
-                }
-                else
-                {
-                    strToRet += origStr[j].ToString().ToLower();
-                }
-            }
-            return strToRet;
+            return fullNameToRet;
         }
     }
+    public int age
+    {
+        get
+        {
+            int age_shadow = DateTime.Now.Year - _birthDay.Year;
+            if (DateTime.Now < _birthDay.AddYears(age_shadow))
+            {
+                age_shadow--; // Adjust if birthday hasn't occurred this year yet
+            }
+            return age_shadow;
+        }
+    }
+    public string address
+    {
+        get
+        {
+            return $"{_houseNumber} {_street}, {_barangay}, {_city}, {_province}, {_country}";
+        }
+    }
+
+    public string firstName
+    {
+        get => _firstName; set
+        {
+            if (value == "")
+            {
+                throw new Exception("Must provide first name input");
+            }
+            if (value.Any(char.IsDigit))
+            {
+                throw new Exception("Number inside of first name");
+            }
+            if (value.Length > 20)
+            {
+                throw new Exception("first name too long. Must be less than or equal to 20");
+            }
+            _firstName = Capitalize(value);
+        }
+    }
+    private string _firstName;
+
+    public string middleInitial
+    {
+        get => _middleInitial; set
+        {
+            if (value.Any(char.IsDigit))
+            {
+                throw new Exception("Number inside of middle initial");
+            }
+            if (value.Length > 10)
+            {
+                throw new Exception("middle initial too long. Must be less than or equal to 10");
+            }
+            _middleInitial = Capitalize(value);
+        }
+    }
+
+    private string _middleInitial;
+
+    public string lastName
+    {
+        get => _lastName; set
+        {
+            if (value == "")
+            {
+                throw new Exception("Must provide last name input");
+            }
+            if (value.Any(char.IsDigit))
+            {
+                throw new Exception("Number inside of last name");
+            }
+            if (value.Length > 20)
+            {
+                throw new Exception("last name too long. Must be less than or equal to 20");
+            }
+            _lastName = Capitalize(value);
+        }
+    }
+    public string _lastName;
+
+    public string suffix
+    {
+        get => _suffix; set
+        {
+            if (value.Any(char.IsDigit))
+            {
+                throw new Exception("Number inside of suffix");
+            }
+            if (value.Length > 10)
+            {
+                throw new Exception("suffix too long. Must be less than or equal to 10");
+            }
+            _suffix = value;
+        }
+    }
+
+    private string _suffix;
+
+    public string birthday
+    {
+        get => _birthDay.ToString("yyyy-MM-dd"); set
+        {
+            if (value == "")
+            {
+                throw new Exception("Must provide birthday input");
+            }
+            string format = "yyyy-MM-dd";
+            _birthDay = DateTime.ParseExact(value, format, System.Globalization.CultureInfo.InvariantCulture);
+        }
+    }
+    public DateTime _birthDay;
+
+    public string houseNumber
+    {
+        get => _houseNumber; set
+        {
+            if (value == "")
+            {
+                throw new Exception("Must provide house number input");
+            }
+            if (value.Length > 10)
+            {
+                throw new Exception("suffix too long. Must be less than or equal to 10");
+            }
+            _houseNumber = value;
+        }
+    }
+    private string _houseNumber;
+
+    public string street
+    {
+        get => _street; set
+        {
+            if (value == "")
+            {
+                throw new Exception("Must provide street input");
+            }
+            if (value.Any(char.IsDigit))
+            {
+                throw new Exception("Number inside of street");
+            }
+            if (value.Length > 20)
+            {
+                throw new Exception("suffix too long. Must be less than or equal to 20");
+            }
+            _street = Capitalize(value);
+        }
+    }
+    private string _street;
+
+    public string barangay
+    {
+        get => _barangay;
+        set
+        {
+            if (value == "")
+            {
+                throw new Exception("Must provide barangay input");
+            }
+            if (value.Any(char.IsDigit))
+            {
+                throw new Exception("Number inside of barangay");
+            }
+            if (value.Length > 20)
+            {
+                throw new Exception("suffix too long. Must be less than or equal to 20");
+            }
+            _barangay = Capitalize(value);
+        }
+    }
+    private string _barangay;
+
+    public string city
+    {
+        get => _city;
+        set
+        {
+            if (value == "")
+            {
+                throw new Exception("Must provide barangay input");
+            }
+            if (value.Any(char.IsDigit))
+            {
+                throw new Exception("Number inside of city");
+            }
+            if (value.Length > 20)
+            {
+                throw new Exception("suffix too long. Must be less than or equal to 20");
+            }
+            _city = Capitalize(value);
+        }
+    }
+    private string _city;
+
+    public string province
+    {
+        get => _province;
+
+        set
+        {
+            if (value == "")
+            {
+                throw new Exception("Must provide province input");
+            }
+            if (value.Any(char.IsDigit))
+            {
+                throw new Exception("Number inside of province");
+            }
+            if (value.Length > 20)
+            {
+                throw new Exception("suffix too long. Must be less than or equal to 20");
+            }
+            _province = Capitalize(value);
+        }
+    }
+    private string _province;
+    public string country
+    {
+        get => _country; set
+        {
+            if (value == "")
+            {
+                throw new Exception("Must provide country input");
+            }
+            if (value.Any(char.IsDigit))
+            {
+                throw new Exception("Number inside of country");
+            }
+            if (value.Length > 20)
+            {
+                throw new Exception("suffix too long. Must be less than or equal to 20");
+            }
+            _country = Capitalize(value);
+        }
+    }
+    private string _country;
+
+    private static string Capitalize(string origStr)
+    {
+        string strToRet = "";
+        bool isFirstChar = true;
+        for (int j = 0; j < origStr.Length; j++)
+        {
+            if (origStr[j] == ' ')
+            {
+                isFirstChar = true;
+                strToRet += " ";
+                continue;
+            }
+            if (isFirstChar)
+            {
+                strToRet += origStr[j].ToString().ToUpper();
+                isFirstChar = false;
+            }
+            else
+            {
+                strToRet += origStr[j].ToString().ToLower();
+            }
+        }
+        return strToRet;
+    }
 }
-
-
