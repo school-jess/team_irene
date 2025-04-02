b82862088b15e177bcbd083f9451240a32f70a6d
school-jess <jessmathew.evangelista@dbtc-cebu.edu.ph>
2025-04-02 16:04:07 +0800

    created initial migrations and applied them

--- pos_library/models/pos.cs
+++ pos_library/models/pos.cs
@@ -1,31 +1,39 @@
-﻿namespace pos_library.models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace pos_library.models;
 
 public class Customers
 {
+    [Key]
     public int customer_id { get; set; }
 }
 
 public class Sale
 {
+    [Key]
     public int sale_id { get; set; }
 }
 
 public class Employee
 {
+    [Key]
     public int employee_id { get; set; }
 }
 
 public class SaleDetail
 {
+    [Key]
     public int sales_detail_id { get; set; }
 }
 
 public class Product
 {
+    [Key]
     public int product_id { get; set; }
 }
 
 public class Inventory
 {
+    [Key]
     public int inventory_id { get; set; }
 }
